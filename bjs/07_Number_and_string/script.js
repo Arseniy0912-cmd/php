a0b5f247bb091c8bd468c4cdd19b9197755a14c2
Arseniy <senya.vlasov.08@gmail.com>
2025-08-19 14:26:52 +0300

    Калькулятор

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,60 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_result').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+
+    if (operation === 'minus') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+
+    if (operation === 'umnoj') {
+        const result = lastOperand * parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+
+    if (operation === 'del') {
+        const result = lastOperand / parseInt(inputWindow.value);
+        lastOperand = 0;
+        operation = null;
+        inputWindow.value = result;
+    }
+})
+
+document.getElementById('btn_minus').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'minus';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_umnoj').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'umnoj';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_del').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'del';
+    inputWindow.value = '';
+})
